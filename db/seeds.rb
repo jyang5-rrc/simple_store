bb503abd13a390468a3be52d3a43b06f1c8424ed
jyang5 <jyang5@academic.rrc.ca>
2024-03-03 21:48:15 -0600

    add validation for Product model and test with seeding invalid data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+invalid_product = Product.new(price: 10, stock_quantity: 5)
+if invalid_product.save
+  puts "Product saved successfully."
+else
+  puts "Failed to save product: #{invalid_product.errors.full_messages.join(", ")}"
+end
