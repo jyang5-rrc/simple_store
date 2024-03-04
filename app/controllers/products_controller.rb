5ec2099109d86c0c03a55f570cf9a89b8df36ad2
jyang5 <jyang5@academic.rrc.ca>
2024-03-04 02:50:30 -0600

    update products index to display categroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,9 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.includes(:category).all # N+1 avoidance means that we want to avoid making a separate database query for each product's category. We can do this by using the includes method to load all the categories at once.
   end
 
   def show
-    @product = Product.find(params[:id])
+    @product = Product.includes(:category).find(params[:id])
   end
 end
